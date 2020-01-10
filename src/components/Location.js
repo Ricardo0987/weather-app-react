0e5d5c7741c9ddce0afea30be2d99a1e246a8f55
Ricardo Ospina <>
2020-01-10 08:45:58 -0500

    Revert "firs commit"
    This reverts commit e3d36f84b6db8e872fa0a18b78275887a8743250.

--- src/components/Location.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Location = () => (
-    <div>
-        <h2> LocaltionComponent - Colombia</h2>
-    </div>
-)
-
-export default Location;
--- src/components/WeatherData.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const WeatherData = () => (
-    <div>WeatherDataComponent</div>
-);
-
-export default WeatherData;
